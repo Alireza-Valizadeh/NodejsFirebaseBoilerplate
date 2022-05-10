9810fa54c3fe1dbdf2b1afd5709aa65607e8c327
Alireza Valizadeh <alirezavalizadeh1999@gmail.com>
2022-05-10 14:12:14 +0430

    Deleted generated test codes

--- src/jest.config.js
+++
@@ -1,8 +0,0 @@
-
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: "/src/__tests__/.*.test.ts",
-  testTimeout: 60000
-};
